3a775d1d7ccf78a837d59c9598e3ddfa024ceda9
Grunt395 <Grantlew@att.net>
2022-11-13 19:00:24 -0800

    Added Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Footer() {
+    const icons = [
+        {
+            name: "fa-brands fa-github",
+            links: "https://github.com/grunt395/"
+        },
+        {
+            name: "fa-brands fa-linkedin",
+            links: "https://www.linkedin.com/in/grant-lewandowski/"
+        }
+    ]
+
+    const LinkStyle = {
+        textDecoration: "none",
+        color: "black", 
+        cursor: "pointer"
+    }
+
+    const FooterStyle = {
+        height: "50px",
+        display: "grid",
+        alignItems: "center",
+        justifyContent: "center",
+        gridGap: "15px"
+    }
+
+
+    return (
+        <footer style={FooterStyle}>
+            <section style={{display: "grid", gridTemplateColumns: "max-content 1fr 1fr", gridGap: "20px"}}>
+                <span style={{paddingRight: "15px"}}> {new Date().getFullYear()}</span>
+                {icons.map(icon => {
+                    return <a href={icon.links} key={icon.name} target="_blank" rel="noopener noreferrer" style={LinkStyle}> <i className={icon.name}></i></a>
+                })}
+            </section>
+        </footer>
+    )
+
+}
+
+export default Footer;
