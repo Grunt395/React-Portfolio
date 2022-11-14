1efba726dcc86e4a4eaf87b79923f52871ed3d19
Grunt395 <Grantlew@att.net>
2022-11-13 20:37:45 -0800

    Navbar click functionality and styles

--- src/App.js
+++ src/App.js
@@ -7,12 +7,26 @@ import Nav from "./components/Nav";
 
 function App() {
   
+  const [pages] = useState([
+    {
+      name: "about"
+    },
+    {
+      name: "portfolio"
+    },
+    {
+      name: "contact"
+    },
+    {
+      name: "resume"
+    },
+  ])
   
-  
+  const [currentPage, setCurrentPage] = useState(pages[0])
   
   return <div>
     <Header>
-        <Nav>
+        <Nav pages={pages} setCurrentPage={setCurrentPage} currentPage={currentPage}>
 
         </Nav>
     </Header>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function Nav() {
-    return <nav>Navbar</nav>
+import "./style.css";
+
+function Nav(props) {
+    const {
+        pages = [],
+        setCurrentPage,
+        currentPage
+    } = props;
+    
+    
+    useEffect(() => {
+        document.title = currentPage.name
+    }, [currentPage])
+    
+    
+    return <nav><ul className="nav-container">
+        {pages.map((page) => {
+            return <li className={currentPage.name === page.name && "active"} key={page.name}><span onClick={() => setCurrentPage(page)}>{page.name}</span></li>
+        })}
+    
+        </ul></nav>
 }
 
 export default Nav;
